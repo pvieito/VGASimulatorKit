f9db56737c17076a6da41a42a2801bb4a46186e0
Pedro José Pereira Vieito <pvieito@gmail.com>
2017-06-07 02:04:22 +0200

    Implemented Application and Icon

---
+++ VGASimulator/VGADocument.swift
@@ -0,0 +1,61 @@
+//
+//  Document.swift
+//  VGASimulator
+//
+//  Created by Pedro José Pereira Vieito on 6/6/17.
+//  Copyright © 2017 Pedro José Pereira Vieito. All rights reserved.
+//
+
+import Cocoa
+import FoundationKit
+import VGASimulatorKit
+
+class VGADocument: NSDocument {
+
+    var vgaSimulation: VGASimulation?
+
+    var documentViewController: VGAViewController? {
+        return self.windowControllers.first?.contentViewController as? VGAViewController
+    }
+
+    enum InputError: LocalizedError {
+        case typeError
+
+        var errorDescription: String? {
+            switch self {
+            case .typeError:
+                return "File not supported."
+            }
+        }
+    }
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+        self.addWindowController(windowController)
+
+        self.documentViewController?.representedObject = vgaSimulation
+    }
+
+    override func read(from url: URL, ofType typeName: String) throws {
+
+        switch typeName {
+        case "com.pvieito.vgasimulation":
+            do {
+                self.vgaSimulation = try VGASimulation(url: url)
+            }
+            catch {
+                throw error.cocoaError
+            }
+        default:
+            throw InputError.typeError.cocoaError
+        }
+    }
+}
+
