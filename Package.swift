e67a4a2b32e3f47df6c8771862a31419a1bb3433
Pedro José Pereira Vieito <pvieito@gmail.com>
2019-11-23 15:01:39 +0100

    [Package] Updated package name

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "VGASimulator",
+    name: "VGASimulatorKit",
     products: [
         .executable(
             name: "VGASimulator",
