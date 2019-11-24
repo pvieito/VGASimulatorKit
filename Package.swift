68a7ff330a1344f47eb412b7829e15cf40d05f18
Pedro José Pereira Vieito <pvieito@gmail.com>
2019-11-24 18:42:51 +0100

    [Package] Updated project

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(path: "../LoggerKit"),
-        .package(path: "../CommandLineKit"),
-        .package(path: "../FoundationKit"),
-        .package(path: "../CoreGraphicsKit")
+        .package(url: "https://github.com/pvieito/CommandLineKit.git", .branch("master")),
+        .package(url: "https://github.com/pvieito/LoggerKit.git", .branch("master")),
+        .package(url: "https://github.com/pvieito/FoundationKit.git", .branch("master")),
+        .package(url: "https://github.com/pvieito/CoreGraphicsKit.git", .branch("master"))
     ],
     targets: [
         .target(
---
+++ README.md
@@ -0,0 +1,17 @@
+# VGASimulatorKit
+
+Swift framework and tool to parse and render frames from VGA simulations.
+
+## Usage
+
+```bash
+$ swift run VGASimulator -i simulation.vga
+```
+
+## Dependencies
+
+- Swift 5.1+
+
+## Note
+
+`VGASimulatorKit` is heavily inspired by [`vga-simulator`](https://github.com/MadLittleMods/vga-simulator).
