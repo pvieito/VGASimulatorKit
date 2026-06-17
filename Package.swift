1bbbe1cbb58d6a44ed7f8b47b462c519013c88b3
Pedro José Pereira Vieito <pvieito@gmail.com>
2026-06-18 00:54:29 +0200

    Update SwiftPM references to main

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "git@github.com:pvieito/LoggerKit.git", branch: "master"),
-        .package(url: "git@github.com:pvieito/FoundationKit.git", branch: "master"),
-        .package(url: "git@github.com:pvieito/CoreGraphicsKit.git", branch: "master"),
+        .package(url: "git@github.com:pvieito/LoggerKit.git", branch: "main"),
+        .package(url: "git@github.com:pvieito/FoundationKit.git", branch: "main"),
+        .package(url: "git@github.com:pvieito/CoreGraphicsKit.git", branch: "main"),
         .package(url: "https://github.com/twostraws/SwiftGD.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
